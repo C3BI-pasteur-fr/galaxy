c1ccacecc41f6e61d668cd37cfbf6072d5751021
fmareuil <fmareuil@pasteur.fr>
2016-03-25 18:19:14 +0100

    feature new restrict function for beta tools

--- lib/galaxy/tools/toolbox/filters/pasteur_filter.py
+++ lib/galaxy/tools/toolbox/filters/pasteur_filter.py
@@ -3,8 +3,12 @@ log = logging.getLogger( __name__)
 
 RESTRICT_TOOLS = []
 
+TEST_USERS = ["fmareuil@pasteur.fr", "odoppelt@pasteur.fr", "mvalade@pasteur.fr"]
+TEST_SECTIONS = ["tests"]
+TEST_TOOLS = []
 
-def restrict_tool_to_pasteuruser( context, tool):
+
+def restrict_tool_to_pasteuruser( context, tool ):
     user = context.trans.user
     if user is not None:
         email = user.email
@@ -14,4 +18,31 @@ def restrict_tool_to_pasteuruser( context, tool):
     if domaine == "pasteur.fr":
         return True
     else:
-        return tool.name not in RESTRICT_TOOLS
+        return tool.id not in RESTRICT_TOOLS
+
+
+def restrict_section_to_notestusers( context, section ):
+    user = context.trans.user
+    if user is not None:
+        login = user.email
+    if section.id in TEST_SECTIONS:
+        if login in TEST_USERS:
+            return True
+        else:
+            return False
+    else:
+        return True
+        
+
+def restrict_tool_to_notestusers( context, tool ):
+    user = context.trans.user
+    if user is not None:
+        login = user.email
+    print tool.id
+    if tool.id in TEST_TOOLS:
+        if login in TEST_USERS:
+            return True
+        else:
+            return False
+    else:
+        return True 
