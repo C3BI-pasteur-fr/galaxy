f3c8be25e59eb39d5acf4c3aaa2ad1b529112e51
fmareuil <fmareuil@pasteur.fr>
2019-02-07 11:27:35 +0100

    Update validation.py
    bug fix: check if metadata exist

--- lib/galaxy/tools/parameters/validation.py
+++ lib/galaxy/tools/parameters/validation.py
@@ -484,7 +484,12 @@ class MetadataInRangeValidator(Validator):
         if value:
             if not isinstance(value, model.DatasetInstance):
                 raise ValueError('A non-dataset value was provided.')
-            value_to_check = int(value.metadata.spec[self.metadata_name].param.to_string(value.metadata.get(self.metadata_name)))
+            try:
+                value_to_check = int(value.metadata.spec[self.metadata_name].param.to_string(value.metadata.get(self.metadata_name)))
+            except KeyError:
+                if self.message is None:
+                    self.message = '{} Metadata missing'.format(self.metadata_name)
+                raise ValueError(self.message)
             try:
                 float(value_to_check)
             except ValueError:
